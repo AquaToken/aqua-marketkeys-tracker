bf1503da78e2300760e6e6b302f0ec209d313c9f
plain-paper <94454728+plain-paper@users.noreply.github.com>
2021-10-20 16:01:24 +0300

    Remove drf secure token task config.

--- aqua_marketkeys_tracker/taskapp/__init__.py
+++ aqua_marketkeys_tracker/taskapp/__init__.py
@@ -18,11 +18,7 @@ app.conf.timezone = 'UTC'
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    from drf_secure_token.tasks import DELETE_OLD_TOKENS
-
     app.conf.beat_schedule.update({
-        'drf_secure_token.tasks.delete_old_tokens': DELETE_OLD_TOKENS,
-
         'aqua_marketkeys_tracker.marketkeys.tasks.task_update_market_keys': {
             'task': 'aqua_marketkeys_tracker.marketkeys.tasks.task_update_market_keys',
             'schedule': crontab(minute='*/5'),
