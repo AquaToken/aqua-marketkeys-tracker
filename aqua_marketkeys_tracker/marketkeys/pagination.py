934b3d41e3565502076a5e519edc352308493ad1
plain-paper <94454728+plain-paper@users.noreply.github.com>
2021-11-17 20:44:05 +0300

    Add count to pagination.

--- aqua_marketkeys_tracker/marketkeys/pagination.py
+++ aqua_marketkeys_tracker/marketkeys/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import CursorPagination
+from rest_framework.response import Response
 
 
 class MarketKeyCursorPagination(CursorPagination):
@@ -14,3 +15,24 @@ class MarketKeyCursorPagination(CursorPagination):
             return self.max_page_size
 
         return super(MarketKeyCursorPagination, self).get_page_size(request)
+
+    def get_count(self, queryset):
+        """
+        Determine an object count, supporting either querysets or regular lists.
+        """
+        try:
+            return queryset.count()
+        except (AttributeError, TypeError):
+            return len(queryset)
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.count = self.get_count(queryset)
+        return super(MarketKeyCursorPagination, self).paginate_queryset(queryset, request, view=view)
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
